b2896c35b4f8462c7b9dd1650a10ca7ff883c029
Flavio <flavio.simonelli.se@gmail.com>
2025-04-16 15:26:32 +0200

    bug fix filament extruder

--- Source code/Marlin/Configuration.h
+++ Source code/Marlin/Configuration.h
@@ -858,7 +858,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 300
+#define EXTRUDE_MAXLENGTH 710
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
